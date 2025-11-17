fc399c2b207218d818739ed041bc042682ba797d
Luke McCormick <lm2000@kiza.com>
2025-11-17 13:18:23 -0500

    Initial Commit

---
+++ BackdropAdmin/BackdropAdminApp.swift
@@ -0,0 +1,17 @@
+//
+//  BackdropAdminApp.swift
+//  BackdropAdmin
+//
+//  Created by Luke McCormick on 11/17/25.
+//
+
+import SwiftUI
+
+@main
+struct BackdropAdminApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
